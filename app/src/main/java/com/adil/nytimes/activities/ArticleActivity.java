014b1a9ba2fca1e868ccbfae5e7408ca07ae150e
Adil Ansari <ansari.adil20@gmail.com>
2016-02-14 21:02:16 -0800

    share intent

--- app/src/main/java/com/adil/nytimes/activities/ArticleActivity.java
+++ app/src/main/java/com/adil/nytimes/activities/ArticleActivity.java
@@ -1,9 +1,15 @@
 package com.adil.nytimes.activities;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -42,5 +48,21 @@ public class ArticleActivity extends AppCompatActivity {
             }
         });
         wvArticle.loadUrl(article.getWebUrl());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_share, menu);
+
+        MenuItem item = menu.findItem(R.id.menu_item_share);
+        ShareActionProvider miShare = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+
+        shareIntent.putExtra(Intent.EXTRA_TEXT, wvArticle.getUrl());
+
+        miShare.setShareIntent(shareIntent);
+        return super.onCreateOptionsMenu(menu);
     }
 }
